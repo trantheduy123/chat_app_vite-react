8e80e609a338511e1ad5e1a380431f2275e272c5
trantheduy123 <trantheduy2309@gmail.com>
2023-11-03 12:15:46 +0700

    finish

--- fontend/src/ChatsPage.jsx
+++ fontend/src/ChatsPage.jsx
@@ -1,5 +1,17 @@
-const ChatsPage = () => {
-  return <>chat...</>;
+import { PrettyChatWinDow } from 'react-chat-engine-pretty';
+
+const ChatsPage = (props) => {
+  return (
+    <div style={{ heigh: '100vh' }}>
+      <PrettyChatWinDow
+        projectId="27ea59e3-713b-4c00-b117-eb8e3fb1a598"
+        // eslint-disable-next-line react/prop-types
+        username={props.user.username}
+        secret={props.user.secret}
+        style={{ height: '100%' }}
+      />
+    </div>
+  );
 };
 
 export default ChatsPage;
